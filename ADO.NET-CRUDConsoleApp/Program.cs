5885736299ca23c3a871654bc38b6497e6c66827
Lucas Quintão <qlucas.dev@gmail.com>
2022-03-01 21:37:43 -0300

    Criando e testando a Query.

---
+++ ADO.NET-CRUDConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿ using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NET_CRUDConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = @"Data Source=LAPTOP-QF5TI23D\SQLEXPRESS;"
+                        + "Initial Catalog=PRODUTOS;"
+                        + "Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(cs);
+            Query query = new Query();
+
+            try
+            {
+                conn.Open();
+                //query.View(conn);
+                //query.Add(conn, "Nave Epacial", 20.55M);
+                //query.Delete(conn, 17);
+                //query.Edit(conn, "MotoRC", 22.99M, 10);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ ADO.NET-CRUDConsoleApp/Query.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NET_CRUDConsoleApp
+{
+    internal class Query
+    {
+        public void View(SqlConnection Conn)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM TB_PRODUTO";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = Conn;
+
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            adapter.Fill(dataSet);
+
+            Console.WriteLine("Total de: {0} registros", dataSet.Tables[0].Rows.Count);
+
+            foreach(DataRow obj in dataSet.Tables[0].Rows)
+            {
+                Console.WriteLine("ID: {0} - Nome: {1} - Preço: {2}", obj["ID"], obj["NOME"], obj["PRECO"]);
+            }
+            
+        }
+
+        public void Add(SqlConnection Conn, string name, decimal price)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "INSERT INTO TB_PRODUTO (NOME, PRECO) VALUES (@name, @price)";
+            cmd.Parameters.Add(new SqlParameter("@name", name));
+            cmd.Parameters.Add(new SqlParameter("@price", price));
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = Conn;
+
+            int nroReg = cmd.ExecuteNonQuery();
+
+            Console.WriteLine(
+                "{0} Registro(s) Inserido(s)",
+                  nroReg.ToString());
+        }
+
+        public void Delete(SqlConnection Conn, int ID)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "DELETE FROM TB_PRODUTO WHERE ID = @ID";
+            cmd.Parameters.Add(new SqlParameter("@ID", ID));
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = Conn;
+
+            int nroReg = cmd.ExecuteNonQuery();
+
+            Console.WriteLine("{0} Registro(s) Deletados(s)", nroReg.ToString());
+        }
+
+        public void Edit(SqlConnection Conn, string name, decimal price, int ID)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "UPDATE TB_PRODUTO SET NOME = @name, PRECO = @price WHERE ID = @id";
+
+            cmd.Parameters.Add(new SqlParameter("@name", name));
+            cmd.Parameters.Add(new SqlParameter ("@price", price));
+            cmd.Parameters.Add(new SqlParameter("@id", ID));
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = Conn;
+
+            int nroReg = cmd.ExecuteNonQuery();
+
+            Console.WriteLine("{0} Registro(s) Alterados(s)", nroReg.ToString());
+        }
+    }
+}
